045ae37ad6317aa119846055c4d5bf5125c8c33f
Miracle <87901620+WaiYanMoeMyintt@users.noreply.github.com>
2024-05-23 09:58:49 +0630

    Create index.js

---
+++ Simple Calculator/index.js
@@ -0,0 +1,20 @@
+function calculator(a,b,sign){
+  
+    if(sign === "+")
+      {
+        return a + b;
+      }
+    else if(sign === "-"){
+        return a - b;
+    }
+    else if(sign === "*"){
+       return a * b;
+    }
+    else if(sign === "/"){
+        return a / b;
+    }
+    else {
+      return "unknown value"
+    }
+
+}
