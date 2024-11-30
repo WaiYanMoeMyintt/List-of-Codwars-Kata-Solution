f1951e4fb5a9dd961cf99c2e2c31c2762ab34627
Miracle <87901620+WaiYanMoeMyintt@users.noreply.github.com>
2024-11-30 17:28:04 +0630

    Create index.js

---
+++ Name on billboard/index.js
@@ -0,0 +1,9 @@
+function billboard(name, price = 30){
+    let totalCost = 0;
+
+    for(let i = 0; i < name.length; i++){
+        totalCost += price;
+    }
+  return totalCost;
+} 
+billboard("Jeong-Ho Aristotelis")
